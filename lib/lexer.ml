3a45bcc9d8cc886a62a691655b93cb18fd2a50e9
Dominik-Pawelec <dominikpawelec@gmail.com>
2024-10-26 14:25:59 +0200

    implementing operators, issue with incorrect signs and managing edge cases

--- lib/lexer.ml
+++ lib/lexer.ml
@@ -2,6 +2,9 @@
 exception Lexer_Error of string
 ;;
 
+let (^^) str chr = str^(String.make 1 chr)  
+;;
+
 
 module LexerMap = Map.Make(String)
 ;;
@@ -11,7 +14,7 @@ type operator =
     | VERT_LINE       | DOUBLE_DOT| EQUAL       | LESS             | MORE              | LESS_EQUAL
     | MORE_EQUAL      | NOT_EQUAL | ALMOST_EQUAL| LESS_ALMOST_EQUAL| MORE_ALMOST_EQUAL
     | NOT_ALMOST_EQUAL| VERY_EQUAL| ASSIGN      | DOTDOTDOT        | FLOOR_LINE
-    | L_PAREN         | R_PAREN   | L_BRACKET   | R_BRACKET
+    | L_PAREN         | R_PAREN   | L_BRACKET   | R_BRACKET        | INVALID_OPERATOR
 ;;
 let operator_map = LexerMap.of_list [
     ("+", PLUS); ("-", MINUS); ("*", MULT); ("/", L_SLASH); ("%", PERCENT); ("&", AND); ("^", POW);
@@ -45,30 +48,45 @@ type stateMachine =
 let getNext input =
     (String.get input 0, String.sub input 1 ((String.length input)-1))
 ;;
-
+let operatorDivisor text =
+    let toTokenOperator text : token = 
+        match LexerMap.find_opt text operator_map with
+            | Some(x) -> Operator(x)
+            | None -> Operator(INVALID_OPERATOR)
+        in
+    let rec scanner_rec text curr_analysed output =
+        if text = "" then if curr_analysed = "" then (toTokenOperator curr_analysed)::output
+                        else output
+        else let next_char = fst (getNext text) and text' = snd (getNext text) in
+        let new_analysed = curr_analysed ^^ next_char in
+        let f_temp x =  LexerMap.is_empty (LexerMap.filter 
+            (fun s _ -> if String.length s < String.length x then false
+            else String.sub s 0 (String.length x) = x)
+            operator_map)
+        in match f_temp new_analysed with
+        | true -> scanner_rec text "" ((toTokenOperator curr_analysed)::output)
+        | false  ->  scanner_rec text' new_analysed output
+    in scanner_rec text "" []
+;;
 let toToken text (state : stateMachine) =
     match state with
-    | Number_Int -> Int(int_of_string text)
-    | Number_Float -> Float(float_of_string text)
-    | String -> String(text)
+    | Number_Int -> [Int(int_of_string text)]
+    | Number_Float -> [Float(float_of_string text)]
+    | String -> [String(text)]
     | Literal ->(match LexerMap.find_opt text keyword_map with
-                | Some(x) -> Keyword(x)
-                | None -> Name(text)
+                | Some(x) -> [Keyword(x)]
+                | None -> [Name(text)]
                 )
-    | Operator -> 
-            Operator(EQUAL) (*TODO:, will be more complicated ("buffor" of operators)*)
-    | _ -> print_endline text; print_endline "^" ;raise(Lexer_Error "toToken: unexpected argument")
-;;
+    | Operator -> operatorDivisor text
 
-(*let operator_signs = ['+';'-';'*';'/';'%';'^';'_';'&';'|';'(';')';'';'';'';'';'';'']*)
-;;
-let (^^) str chr = str^(String.make 1 chr)  
+    | _ -> [] 
 ;;
 let tokenizer input = 
     let rec tokenizer_rec input curr_analysed state output =
-        if input = "" && curr_analysed != "" && curr_analysed != " " && curr_analysed != "\n" && curr_analysed != "\t" 
-            then (toToken curr_analysed state)::output
-        else if input = "" || input = " " then output (*TODO: Correct sanitisation of empty text*) 
+        if input = "" && curr_analysed != "" && curr_analysed != " " 
+        && curr_analysed != "\n" && curr_analysed != "\t" 
+            then (toToken curr_analysed state)@output
+        else if input = ""  then output (*TODO: Correct sanitisation of empty text*) 
         else match state with
         | Empty -> let next_char = fst (getNext input) and input' = snd (getNext input) in
                 ( match next_char with
@@ -81,17 +99,17 @@ let tokenizer input =
         | Literal -> let next_char = fst (getNext input) and input' = snd (getNext input) in
                 ( match next_char with
                 | 'a'..'z'|'A'..'Z' -> tokenizer_rec input' (curr_analysed ^^ next_char) state output
-                | _ -> tokenizer_rec input "" Empty ((toToken curr_analysed state)::output)
+                | _ -> tokenizer_rec input "" Empty ((toToken curr_analysed state)@output)
                 )
         | String -> let next_char = fst (getNext input) and input' = snd (getNext input) in
                 ( match next_char with
-                | '"' -> tokenizer_rec input' "" Empty ((toToken curr_analysed state)::output)
+                | '"' -> tokenizer_rec input' "" Empty ((toToken curr_analysed state)@output)
                 | _ -> tokenizer_rec input' (curr_analysed ^^ next_char) state output
                 )
         | Operator -> let next_char = fst (getNext input) and input' = snd (getNext input) in
                 ( match next_char with 
                 | 'a'..'z'|'A'..'Z'|'"'|'0'..'9'|' '|'\n'|'\t' -> 
-                        tokenizer_rec input "" Empty ((toToken  curr_analysed state)::output)
+                        tokenizer_rec input "" Empty ((toToken curr_analysed state)@output)
                 | _ -> tokenizer_rec input' (curr_analysed ^^ next_char) state output
                 )
         | Number_Int -> let next_char = fst (getNext input) and input' = snd (getNext input) in
@@ -99,25 +117,14 @@ let tokenizer input =
                 | '0'..'9' -> tokenizer_rec input' (curr_analysed ^^ next_char) state output
                 | '.' -> (match fst (getNext input') with(*make more secure, currently 1. makes very bad things*)
                         | '0'..'9' -> tokenizer_rec input' (curr_analysed ^^ next_char) Number_Float output
-                        | _ -> tokenizer_rec input "" Empty ((toToken curr_analysed Number_Int)::output)
+                        | _ -> tokenizer_rec input "" Empty ((toToken curr_analysed Number_Int)@output)
                         )
-                | _ -> tokenizer_rec input "" Empty ((toToken curr_analysed Number_Int)::output)
+                | _ -> tokenizer_rec input "" Empty ((toToken curr_analysed Number_Int)@output)
                 )
         | Number_Float -> let next_char = fst (getNext input) and input' = snd (getNext input) in
             (match next_char with
             | '0'..'9' -> tokenizer_rec input' (curr_analysed ^^ next_char) state output
-            | _ -> tokenizer_rec input "" Empty ((toToken curr_analysed Number_Float)::output)
+            | _ -> tokenizer_rec input "" Empty ((toToken curr_analysed Number_Float)@output)
             )
     in List.rev (tokenizer_rec input "" Empty [])
 ;;
-
-
-
-
-
-
-
-
-
-
-
